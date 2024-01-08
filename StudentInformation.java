09e5d1e1df3a666290d1214a8b18b6ec8a86eb25
puppy1289 <151377190+puppy1289@users.noreply.github.com>
2024-01-08 15:59:39 +0530

    Update StudentInformation.java

--- StudentInformation.java
+++ StudentInformation.java
@@ -12,7 +12,7 @@ int rollno=1289;
 System.out.println("collegename:"+collegename);
 System.out.println("studentname:"+studentname);
 System.out.println("rollno:"+rollno);
-System.out.println("\n welcome ,+studentname+"!");
+System.out.println("\n welcome ",+studentname+"!");
 System.out.println("we hope you have a fantastic journey at"+collegename+".");
 System.out.println("kill the sleeping people");
 }
