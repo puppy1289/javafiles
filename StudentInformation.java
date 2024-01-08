9a863df09f6902498157cd75a9dce6bccf6c168f
root <root@ip-172-31-47-222.ec2.internal>
2024-01-08 10:12:06 +0000

    student information

---
+++ StudentInformation.java
@@ -0,0 +1,19 @@
+public class StudentInformation
+{
+public static void main(String args[])
+{
+printStudentinfo
+}
+private static void printstudentinfo()
+{
+String studentname="abhi";
+String collegename="aits";
+int rollno=1289;
+System.out.println("collegename:"+collegename);
+System.out.println("studentname:"+studentname);
+System.out.println("rollno:"+rollno);
+System.out.println("\n welcome ,+studentname+"!");
+System.out.println("we hope you have a fantastic journey at"+collegename+".");
+System.out.println("kill the sleeping people");
+
+
