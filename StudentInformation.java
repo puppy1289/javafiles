9c3beceda366a05f0228028254b1cd3b1f5cd300
puppy1289 <151377190+puppy1289@users.noreply.github.com>
2024-01-08 16:04:43 +0530

    Update StudentInformation.java

--- StudentInformation.java
+++ StudentInformation.java
@@ -4,7 +4,7 @@ public static void main(String args[])
 {
 printStudentinfo();
 }
-private static void printstudentinfo()
+private static void printStudentinfo()
 {
 String studentname="abhi";
 String collegename="aits";
